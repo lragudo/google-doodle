af6f4b975c2acc2595a78c5b67d8bed290879fd3
Lillian <59583025+lragudo@users.noreply.github.com>
2020-02-24 10:30:14 -0800

    update favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
-gsap.from("#plain20", {
-    y: 200,
-    duration: 1.25,
-    rotation: 200,
-    delay: 1.5
-  })
-  
-  gsap.from("#pencil", {
-    y:-300,
-    duration: 1.25,
-    delay: 1.75
-  })
-
 gsap.registerPlugin(MotionPathPlugin);
+
 gsap.to("#nat20", {
   duration: 2.5,
   rotation: 360,
@@ -21,4 +9,17 @@ gsap.to("#nat20", {
     end: .95
   }
 });
+
+gsap.from("#plain20", {
+  y: 200,
+  duration: 1.25,
+  rotation: 200,
+  delay: 1.5
+})
+
+gsap.from("#pencil", {
+  y:-300,
+  duration: 1.25,
+  delay: 1.75
+})
 
