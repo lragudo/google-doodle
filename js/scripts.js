832c6c9432c50a9eadc40978420cd05477937758
Lillian <59583025+lragudo@users.noreply.github.com>
2020-02-25 20:20:08 -0800

    updated and added the next part from my codpen pens
    Added a second scene with the title 'International Tabletop Day'

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 gsap.registerPlugin(MotionPathPlugin);
 
+// dice role in as well as the pencil
 gsap.to("#nat20", {
   duration: 2.5,
   rotation: 360,
@@ -7,8 +8,7 @@ gsap.to("#nat20", {
    motionPath:{
     path: "#path",
     end: .95
-  }
-});
+  }});
 
 gsap.from("#plain20", {
   y: 200,
@@ -23,3 +23,57 @@ gsap.from("#pencil", {
   delay: 1.75
 })
 
+// Remove red dice
+gsap.to("#nat20", {
+  duration: 2.5,
+  rotation: 200,
+  delay: 4.5,
+  x: -100,
+  y: 400  
+})
+
+// Remove yellow dice
+gsap.to("#plain20", {
+  duration: 2.5,
+  rotation: 300,
+  delay: 4.5,
+  x: -100,
+  y: -400  
+})
+
+// Remove pencil
+gsap.to("#pencil", {
+  delay: 4.5,
+  rotation: 150,
+  x: 190,
+  y: 100,
+  duration: 1.25
+})
+
+// Remove gge
+gsap.to(".google", {
+  opacity: 0,
+  delay: 4.5
+})
+
+// international title comes in from above
+gsap.fromTo("#international", {
+  opacity: 0,
+  y: -75
+},{
+  opacity: 1,
+  duration: 3,
+  y: 150,
+  delay: 5
+})
+
+//table-top day comes in from below
+gsap.fromTo("#tabletop-day", {
+  opacity: 0,
+  y: 75
+},{
+  opacity: 1,
+  duration: 3,
+  y: -140,
+  delay: 5
+})
